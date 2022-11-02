2d68840df3d3ffb1cf7480001d74b8bdfb3c0b3b
naruto <73755581+naruto-o@users.noreply.github.com>
2022-11-02 13:13:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let a = "shikhar";
+let b = 6;
+console.log(a + b);
+console.log(typeof (a + b));
+const a1 = {
+  name: "shikahr",
+  age: "18",
+  isPrincipal: false
+}
+// a1 = 45; will throw an error
+a1["friend"] = "shashasnk"
+a1["section"] = "aiml"
+console.log(a1)
+
+const dict = {
+  appreciate: "comliment",
+  yakka: "hard work",
+  yoyo: "honey singh"
+}
+console.log(dict.yakka)
+console.log(dict)
+console.log(dict['appreciate'])
