7bef66683b209129e6739a17c278a9ae4943f0d6
naruto <73755581+naruto-o@users.noreply.github.com>
2022-11-02 13:14:41 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,20 @@
+let age  = prompt("what is your age?")
+if(age>10 && age<20){
+  console.log("age lies between 10 and 20")
+}
+else{
+  console.log("age does not lie between 10 adn 20")
+}
+let age  = prompt("what is your age?")
+switch(age){
+  case 12:
+    console.log("your  age is 12")
+   case 14:
+    console.log("your  age is 14")
+   case 16:
+    console.log("your  age is 16")
+   case 18:
+    console.log("your  age is 18")
+  default:
+    console.log("your age is not wspecial")
+}
