00f9943a76fb1947dbb46871831ee9a295464a13
naruto <73755581+naruto-o@users.noreply.github.com>
2022-11-17 00:02:00 +0530

    Create js_loops.js

---
+++ js_loops.js
@@ -0,0 +1,12 @@
+let  obj  = {
+  harry:90,
+  ss:80,
+  smith:60,
+  wesson:77
+}
+for(let a in obj){
+  console.log("marks of " + a +  " are" + " "+obj[a]);
+}
+for(let  b of "Harry"){
+  console.log(b);
+}
