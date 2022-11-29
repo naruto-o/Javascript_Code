801344fab31b938deaaf3903be17bcaf6f905f9e
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2022-11-30 00:35:12 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,11 @@
+console.log('heello world');
+const prompt = require("prompt-sync")();ṇ
+food  = prompt("amount of the food eaten --");
+tippercentage = 0.2;
+Total_Amount = food * tippercentage;
+Total_Amount = Total_Amount + food;
+console.log(Total_Amount);
+
+// User Input
+const prompt1 = require("prompt-sync")();
+var name = prompt("What is your name");
