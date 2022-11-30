57fa8c3152dcc0db6379d100918c009e2969d2be
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2022-12-01 01:39:16 +0530

    Add files via upload

---
+++ js7.js
@@ -0,0 +1,13 @@
+const lettercounter = () =>{
+    const phrase = 'hey,can you go to the zoro website!'
+    let temp = 0;
+    for( const letter in phrase ){
+        temp++;
+    }
+    console.log(`{result is '${temp}'}`);    
+}
+// console.log(temp);
+lettercounter();  
+let a = 'banana';
+console.log(a.length);
+console.log('logan'.length);
