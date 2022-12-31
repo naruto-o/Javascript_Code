ff9558b942d5134a7414d0a4ca1b5bf1dbcdbf51
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2023-01-01 02:05:46 +0530

    Add files via upload
    use it with increment in html to do a increment counter

---
+++ increment.js
@@ -0,0 +1,13 @@
+let count  =0;
+function increment(){
+    count++;
+    console.log(count);
+    document.getElementById('el-count').innerHTML = count
+}
+let clearCount = 0;
+function clearScreen(){
+    document.getElementById('el-count').innerHTML =     clearCount
+    count = 0;
+}
+console.log("yooyoootfdcvgfrdf")
+
