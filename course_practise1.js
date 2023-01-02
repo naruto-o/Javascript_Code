67739291ecc8211a92edd705568a0251505aef67
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2023-01-03 00:58:52 +0530

    Add files via upload

---
+++ course_practise1.js
@@ -0,0 +1,6 @@
+let render = document.getElementById("error");
+console.log(render);
+function rendermessage(){
+    render.textContent = "Something went wrong,Please try again after some time"
+    console.log("button clicked of purchase");
+}
